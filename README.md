e970d7d1884622bf62d77f1e34a3151d5dc936b7
suppergerrie2 <15769860+suppergerrie2@users.noreply.github.com>
2022-10-13 18:23:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sapiens Lua Mod Template
+
+A template for Sapiens mod using Lua.
+
+Provides a minimal `modInfo.lua`.
+
+For more information check out the [wiki](https://wiki.sapiens.dev/)
---
+++ modInfo.lua
@@ -0,0 +1,10 @@
+local modInfo = {
+    name = "My Mod Name",
+    description = "This is a minimal mod example",
+    -- preview = "preview.jpg", -- Optional
+    -- version = "0.0.1", -- Optional
+    type = "world", -- or "app"
+    developer = "Me", 
+    -- website = "https://github.com/suppergerrie2/SapiensLuaModTemplate", -- Optional
+}
+return modInfo
